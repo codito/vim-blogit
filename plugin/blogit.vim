codito/vim-blogit

commit f57af81fe710842abf9bee2e73c69a1dfcdd6fde

display_comment_count: Fixed for :Blogit new.
* handle d='' (as with command_new; new status 'new')

* Simplified code via list-comprehension.

plugin/blogit.vim
@@ -201,15 +201,16 @@ class BlogIt:
 
     def display_post(self, post={}, new_text=None):
         def display_comment_count(d):
-            if d['awaiting_moderation'] > 0:
-                if d['spam'] > 0:
-                    s = u' (%(awaiting_moderation)s awaiting, %(spam)s spam)'
-                else:
-                    s = u'%(awaiting_moderation)s'
-            elif d['spam'] > 0:
-                s = u' (%(spam)s spam)'
-            else:
+            if d == '':
+                return u'new'
+            comment_typ_count = [ '%s %s' % (key, text) 
+                    for key, text in ( ( 'awaiting_moderation', 'awaiting' ),
+                            ( 'spam', 'spam' ) ) 
+                    if d[key] > 0 ]
+            if comment_typ_count == []:
                 s = u''
+            else:
+                s = u' (%s)' % ', '.join(comment_typ_count)
             return ( u'%(post_status)s – %(total_comments)s Comments' + s ) % d
 
         default_post = { 'post_status': 'draft',

commit afb3d7b413a049cc3ed7a6b7dbdd259729322cda

Fixed comment count.
Affected releases: 1.2.

Bug introduced in f57af81fe710842abf9bee2e73c69a1dfcdd6fde.

plugin/blogit.vim
@@ -285,7 +285,7 @@ class BlogIt:
         def display_comment_count(d):
             if d == '':
                 return u'new'
-            comment_typ_count = [ '%s %s' % (key, text)
+            comment_typ_count = [ '%s %s' % (d[key], text)
                     for key, text in ( ( 'awaiting_moderation', 'awaiting' ),
                             ( 'spam', 'spam' ) )
                     if d[key] > 0 ]